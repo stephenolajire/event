c461276e0054194281035f47fa00fde10fd0e765
olajirestephen <horlharmighty2000@gmail.com>
2026-02-08 13:21:52 -0600

    addedd

--- backend/event/settings.py
+++ backend/event/settings.py
@@ -200,10 +200,7 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 # Email Settings
-EMAIL_BACKEND = os.environ.get(
-    'EMAIL_BACKEND',
-    'django.core.mail.backends.smtp.EmailBackend'
-)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
